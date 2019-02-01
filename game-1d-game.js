a97075aa7365d97833cfa1f2c0d993d11a8bef0d
qq200 <43753261+qq200@users.noreply.github.com>
2019-02-01 18:00:22 +0200

    Add files via upload

--- game-1d-game.js
+++ game-1d-game.js
@@ -4,6 +4,7 @@ var position = 7; // personaj, caracter position
 // +------ 
 function showMap(){
     document.body.innerHTML= '';
+    if (p>0 && p<MAP_SIZE){
     for(var p=1; p<=MAP_SIZE; p++){
         if(p == position){
             document.write('+');
@@ -12,15 +13,48 @@ function showMap(){
         }
     }
     document.write(`
-    <button>&lt</button>
+    <button >&lt</button>
     <button onclick="moveRight();showMap();">&gt</button>
     
-    `)
+    `)}else{
+    for(var p=10; p>0 ; p--){
+            if(p == position){
+                document.write('+');
+            }else{
+            document.write('-');
+            }
+        }
+        document.write(`
+        <button onclick="moveLeft();showMap();">&lt</button>
+        <button>&gt</button>
+        
+        `)}
+
+
+
 }
 
+
 function moveRight(){
+    // document.body.innerHTML= '';
+    // for(var p=10; p<=MAP_SIZE && p>0 ; p--){
+    //     if(p == position){
+    //         document.write('+');
+    //     }else{
+    //     document.write('-');
+    //     }
+    // }
+    // document.write(`
+    // <button onclick="moveRight();showMap();">&lt</button>
+    // <button>&gt</button>
+    
+    // `)
     position++;
 
 
 }
-showMap()
+function moveLeft(){
+    position--;
+}
+
+showMap();
