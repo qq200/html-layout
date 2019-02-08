d477e13bca2e13de797ddc0310dbc7bb31c835a8
qq200 <43753261+qq200@users.noreply.github.com>
2019-02-08 13:08:38 +0200

    Add files via upload

--- game-oop.js
+++ game-oop.js
@@ -3,11 +3,16 @@ let dino={
     jumping:false, //sare sau nu
     ///shows the dino
     render: function(){
-
+        if(dino.jumping==true){
         //if/else sa indeplineasca doar daca jumping este true
        let html= ` <div class="dino animated bounce"></div>`;
        let div= document.getElementById('land');
             div.innerHTML=html;
+        }else{
+            let html= ` <div class="dino"></div>`;
+            let div= document.getElementById('land');
+            div.innerHTML=html;
+            }
     }
 };
 
